c835b9c8e0699ec358dcdd99ca878dfb57c0be00
tehmongoose <alex.dodson@gmail.com>
2017-02-21 23:50:54 -0500

    methodzzz

--- pet-service-document-store/src/main/java/com/efx/pet/service/temp/MemberCenterProductCatalog.java
+++ pet-service-document-store/src/main/java/com/efx/pet/service/temp/MemberCenterProductCatalog.java
@@ -6,7 +6,8 @@ import java.util.Map;
 
 public class MemberCenterProductCatalog {
 
-    private String catalogName;
+    private String displayName;
+    private String key;
     private Map<String, MemberCenterProduct> products;
 
     public MemberCenterProductCatalog() {
@@ -33,5 +34,42 @@ public class MemberCenterProductCatalog {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        MemberCenterProductCatalog that = (MemberCenterProductCatalog) o;
+
+        return key.equals(that.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode();
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Map<String, MemberCenterProduct> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Map<String, MemberCenterProduct> products) {
+        this.products = products;
+    }
 }
